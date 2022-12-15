6821a1f02aba71221b0987fcc99c76738db33574
TuplexGaming <99618518+TuplexGaming@users.noreply.github.com>
2022-12-15 16:53:00 -0700

    Initial commit from Dec 11 stream.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Advent-of-Code-2022
+# Advent of Code 2022
+
 Python solutions for Advent of Code 2022.
+
+Streamed live on [Twitch](https://www.twitch.tv/tuplex).
---
+++ template.py
@@ -0,0 +1,17 @@
+def get_input(filename):
+    with open(filename) as file:
+        data = [line.strip() for line in file]
+    return data
+
+
+def part_1():
+    pass
+
+
+def part_2():
+    pass
+
+
+if __name__ == "__main__":
+    print("Part 1: ", part_1())
+    print("Part 2: ", part_2())
